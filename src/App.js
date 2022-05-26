017513d3c1586f60b27f71b4b5be6254c25f6e44
unknown <zaeemaftabsiddiqui@gmail.com>
2022-05-26 17:05:56 +0500

    delete extra li

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
-            Navbar
+            Textutils
           </a>
           <button
             className="navbar-toggler"
@@ -28,43 +28,8 @@ function App() {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="/">
-                  Link
+                  About
                 </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
               </li>
             </ul>
             <form className="d-flex" role="search">
