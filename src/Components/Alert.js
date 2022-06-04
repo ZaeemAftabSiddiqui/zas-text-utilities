40e76eeb4505ced84dbe51ed452f32b5c2d7c2a6
unknown <zaeemaftabsiddiqui@gmail.com>
2022-06-04 15:27:23 +0500

    creat alert and add favicon

---
+++ src/Components/Alert.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Alert(props) {
+  const capitalize = (word) => {
+    const lower = word.toLowerCase();
+    return lower.charAt(0).toUpperCase() + lower.slice(1);
+  };
+  return (
+    // this is like a if and else ....if this is true then whole function run
+    props.alert && (
+      <div
+        className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+        role="alert"
+      >
+        <strong>{capitalize(props.alert.type)}</strong>: {props.alert.msg}
+      </div>
+    )
+  );
+}
+
+export default Alert;
