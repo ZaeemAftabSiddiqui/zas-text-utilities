0914d0e0ae4982ecaf48a4c9a1c8374bb2699e94
unknown <zaeemaftabsiddiqui@gmail.com>
2022-06-06 12:49:40 +0500

    react dom added and working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import { useState } from "react";
 import Alert from "./Components/Alert";
-//import About from "./Components/About";
+import About from "./Components/About";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //https://wordcounter.net/ just like these sites
 //https://www.online-utility.org/text/analyzer.jsp
@@ -24,26 +26,44 @@ function App() {
       setMode("dark");
       document.body.style.backgroundColor = "#09492b";
       showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+      // this is to blink title
+      // setInterval(() => {
+      //   document.title = "TextUtils is amazing";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils now";
+      // }, 1500);
     } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode";
     }
   };
   return (
-    <>
-      {/* <Navbar title="TextUtils" aboutText="About us" /> */}
+    <Router>
+      {/* <Navbar title="TextUtils" aboutText="About us"
+         /> */}
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div className="container my -3">
-        <TextForm
-          showAlert={showAlert}
-          heading="Enter your text to analyze below"
-          mode={mode}
-        />
-        {/* <About /> */}
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route
+            exact
+            path="/"
+            element={
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter your text to analyze below"
+                mode={mode}
+              />
+            }
+          />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
 
